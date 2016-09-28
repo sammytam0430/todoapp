b45c87bedfec281c12a526ea75c33cf572459524
Sammy Tam <sammytam2005@hotmail.com>
2016-09-28 21:30:10 +0000

    modify connection

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,17 @@
 // Update with your config settings.
+const settings = require("../settings"); // settings.json
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database,
+      host     : settings.hostname,
+      port     : settings.port,
+      ssl      : settings.ssl
     }
   },
 
