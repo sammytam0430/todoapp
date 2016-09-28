dc20e97e6ad19be5f6be7381cc6e37bb71a826c2
Sammy Tam <sammytam2005@hotmail.com>
2016-09-28 22:40:55 +0000

    updated with modules requiremnt

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const settings = require("../settings"); // settings.json
+require('dotenv').config();
 var knex = require('knex')({
   client: 'pg',
   connection: {
-    user     : settings.user,
-    password : settings.password,
-    database : settings.database,
-    host     : settings.hostname,
-    port     : settings.port,
-    ssl      : settings.ssl
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+    host     : process.env.DB_HOST,
+    port     : process.env.DB_POST,
+    ssl      : process.env.DB_SSL
   }
 });
+
+const PORT        = process.env.PORT || 8080;
+const express     = require("express");
+const bodyParser  = require("body-parser");
+const app         = express();
