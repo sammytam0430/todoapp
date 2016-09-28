ef3a4a4634ae3fb0a831965d9f6e07ec2952dc35
Sammy Tam <sammytam2005@hotmail.com>
2016-09-28 19:58:08 +0000

    setup up knex settings

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const settings = require("./settings"); // settings.json
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
+});
