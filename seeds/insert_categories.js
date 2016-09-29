c8dc46753d0bc20d5c87c689837ec23fd23928b3
Sammy Tam <sammytam2005@hotmail.com>
2016-09-29 02:30:33 +0000

    inserted data into tables with knex seed

--- seeds/insert_categories.js
+++ seeds/insert_categories.js
@@ -1,10 +1,14 @@
-
-exports.seed = function(knex, Promise) {
-  return Promise.all([
-    // Inserts seed entries
-    knex('categories').insert({id: 1, name: 'watch'}),
-    knex('categories').insert({id: 2, name: 'eat'}),
-    knex('categories').insert({id: 3, name: 'read'}),
-    knex('categories').insert({id: 4, name: 'buy'})
-  ]);
-};
+//
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex('categories').del()
+//     .then(function () {
+//       return Promise.all([
+//         // Inserts seed entries
+//         knex('categories').insert({id: 1, name: 'Watch'}),
+//         knex('categories').insert({id: 2, name: 'Eat'}),
+//         knex('categories').insert({id: 3, name: 'Read'}),
+//         knex('categories').insert({id: 4, name: 'Buy'})
+//       ]);
+//     });
+// };
