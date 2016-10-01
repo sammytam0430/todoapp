0ac567650118b313425aac9126549df2a42bd071
Sarah Veness <sarahhveness@gmail.com>
2016-10-01 01:21:33 +0000

    updated form

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,18 +1,50 @@
 $(document).ready(function() {
-$(document).foundation()
+// $(document).foundation()
 
-    $("#buy, #eat, #read, #watch").click(function(event) {
-        $(this).next('.togglecontainer').slideToggle();
-    });
+const show = require('../lib/select');
+
+$("#buy, #eat, #read, #watch").click(function(event) {
+  $(this).next('.togglecontainer').slideToggle();
 });
-  // $('#submit').click(function() {
-  //   $('.togglecontainer').slidetoggle("slow", function() {
-  //   });
-  // });
 
-// var hiddenBox = $( "togglecontainer" );
-// $( "#togglecontainer" ).on( "click", function( event ) {
-//   hiddenBox.show();
-// });
+function newToDo(item_name, item_category, html_block, status) {
+    var toDo = $('${<ul class="listcontainer">}')//.addClass('tweet');
+    toDo.append($(`
+      <div class="namecontainer">
+        <h2>"${taskObject.name}</h2>
+        <input type="image" src="../images/check.png" id="${taskType}">
+          <div class="togglecontainer">
+            <li>
+              <div class="item-content">
+                <img src="${taskObject.img}"/>
+                <div class="synopsis" id="scrollContainer">${taskObject.desc}
+                </div>
+                <p class="rating">${taskObject.rating}</p>
+                <a href="${taskObject.url}" id="url">View the trailer here</a>
+                <div class="buttons">
+                  <p><input type="image" src="../images/check.png" class="sidebuttons delete">
+                  <p><input type="image" src="../images/bird.png" class="sidebuttons complete">
+                </div>
+              </div>
+            </li>
+          </div>
+      </div>`
+      ));
+    return toDo;
+  };
 
-  // console.log("hello");
+
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    var tweet = $(this).serialize();
+    $.ajax( {
+      url: '/tweets',
+      method: 'POST',
+      data: tweet,
+      dataType: 'json',
+      success: function() {
+         $('#tweetcontai').append(newToDo(cb));
+      }
+    });
+  });
+});
