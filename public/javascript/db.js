ad4d69d002bc9ad967065ca65289965748a12c61
Sammy Tam <sammytam2005@hotmail.com>
2016-10-02 01:02:18 +0000

    db jquery

---
+++ public/javascript/db.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  $('#todocontainer').find('#read').on('click', function(e) {
+    e.preventDefault();
+    const $this = $(this).parents('.listcontainer')
+    console.log($this);
+  });
+});
