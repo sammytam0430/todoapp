c5865175f90a7edfaacf5e348d22c258f8c389d1
Sammy Tam <sammytam2005@hotmail.com>
2016-10-05 08:16:34 +0000

    refactored/ use strict

---
+++ seeds/add_users.js
@@ -0,0 +1,13 @@
+//
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex('users').del()
+//     .then(function () {
+//       return Promise.all([
+//         // Inserts seed entries
+//         knex('users').insert({user_id: 1, user_name: 'Sammy'}),
+//         knex('users').insert({user_id: 2, user_name: 'Sarah'}),
+//         knex('users').insert({user_id: 3, user_name: 'Mitch'})
+//       ]);
+//     });
+// };
